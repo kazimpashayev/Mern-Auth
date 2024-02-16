5787a2b1c7558455143b9896132c25d50e190c48
kazimpashayev <kazimpasayev@protonmail.com>
2024-02-16 20:23:37 +0400

    Completing sign up functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,33 +1,71 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      const response = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+      setLoading(false);
+
+      if (data.status === 'fail') {
+        setError(true);
+        return;
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4" action="">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           className="bg-gray-200 p-3 rounded-lg"
           type="text"
           placeholder="Username"
           id="username"
+          onChange={handleChange}
         />
         <input
           className="bg-gray-200 p-3 rounded-lg"
           type="email"
           placeholder="Email"
           id="email"
+          onChange={handleChange}
         />
         <input
           className="bg-gray-200 p-3 rounded-lg"
           type="password"
           placeholder="Password"
           id="password"
+          onChange={handleChange}
         />
         <button
+          disabled={loading}
           type="submit"
           className="bg-gray-800 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80"
         >
-          Sign Up
+          {loading ? 'Loading...' : 'Sign Up'}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
@@ -36,6 +74,7 @@ export default function SignUp() {
           <Link to="/sign-in">Sign in</Link>
         </span>
       </div>
+      <p className="text-red-700 mt-5">{error && 'Something went wrong!'}</p>
     </div>
   );
 }
