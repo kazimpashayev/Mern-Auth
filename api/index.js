03e757b927a00578f7c7062bf4dcbb95efded7ab
kazimpashayev <kazimpasayev@protonmail.com>
2024-02-25 17:53:10 +0400

    Creating update user api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import authRoutes from './routes/authRoute.js';
+import userRoutes from './routes/userRoute.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 mongoose
@@ -16,12 +18,14 @@ mongoose
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000!');
 });
 
 app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
