31a5731d08a881200d4dffa5625e2fb5e98fdd54
kazimpashayev <kazimpasayev@protonmail.com>
2024-03-01 19:52:45 +0400

    Copleting home and about pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,29 @@
 import React from 'react';
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="px-2 py-12 max-w-2xl mx-auto">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold mb-4 text-gray-800">About</h1>
+        <p className="mb-4 text-gray-700 text-lg">
+          I've crafted this application using the MERN stack, integrating
+          MongoDB, Express, React, and Node.js to ensure a seamless and
+          efficient experience for users.
+        </p>
+        <p className="mb-4 text-gray-700 text-lg">
+          At its core, my application prioritizes user authentication, offering
+          both sign-in and sign-up functionalities. Additionally, I've
+          implemented profile image uploading capabilities using Firebase,
+          allowing you to personalize your account with ease.
+        </p>
+        <p className="mb-4 text-gray-700 text-lg">
+          Your privacy and control are important to me. That's why I've included
+          features such as account deletion, ensuring that you have full control
+          over your data and account settings. And when you're ready to step
+          away, signing out is just a click away, providing peace of mind
+          knowing that your session is securely closed.
+        </p>
+      </div>
+    </div>
+  );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,34 @@
 import React from 'react';
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="px-2 py-12 max-w-2xl mx-auto">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold mb-4 text-gray-800">
+          Welcome to my Auth App!
+        </h1>
+        <p className="mb-4 text-gray-700 text-lg">
+          I've developed a comprehensive full-stack web application using the
+          MERN (MongoDB, Express, React, Node.js) stack. This application has
+          strong authentication features that allow users to easily sign up,
+          sign in and sign out. Additionally, it allows users to add a profile
+          photo and delete their account. It also provides access to protected
+          routes for authenticated users only.
+        </p>
+        <p className="mb-4 text-gray-700 text-lg">
+          As a developer, I've constructed the frontend of the application using
+          React, with client-side routing simplified by React-Router-Dom. On the
+          backend, I've used Node.js and Express, with MongoDB serving as the
+          database solution. For authentication, I've implemented JSON Web
+          Tokens (JWT) to ensure secure user access.
+        </p>
+        <p className="mb-4 text-gray-700 text-lg">
+          This application is designed to serve as a foundation for developing
+          full-stack web applications with authentication using the MERN stack.
+          You're welcome to use it as a template for your own projects and
+          customize it according to your requirements!
+        </p>
+      </div>
+    </div>
+  );
 }
